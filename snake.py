a3405f31c3ca4df376b1578209cf32ece3ab096a
bllgg <gunathilakebllg@gmail.com>
2020-06-06 06:18:26 +0530

    close button

--- snake.py
+++ snake.py
@@ -1,12 +1,13 @@
 import pygame
 pygame.init()
-dis=pygame.display.set_mode((400,300))
+dis=pygame.display.set_mode((800,600))
 pygame.display.update()
 pygame.display.set_caption('Snake game')
 game_over=False
 while not game_over:
     for event in pygame.event.get():
-        print(event)   #prints out all the actions that take place on the screen
+        if event.type==pygame.QUIT:
+            game_over=True
  
 pygame.quit()
 quit()
