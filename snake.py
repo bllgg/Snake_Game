6501857bb31b9667ad11189980f3d35d7853293e
bllgg <gunathilakebllg@gmail.com>
2020-06-06 14:50:30 +0530

    Added food

--- snake.py
+++ snake.py
@@ -1,68 +1,95 @@
 import pygame
 import time
-
+import random
+ 
 pygame.init()
-dis=pygame.display.set_mode((800,600))
-pygame.display.update()
-pygame.display.set_caption('Snake game')
-
-blue=(0,0,255)
-red=(255,0,0)
-green = (0,255,0)
-black = (0,0,0)
+ 
 white = (255, 255, 255)
-
-game_over=False
-
-x1 = 400
-y1 = 300
-
-snake_block = 20
-
-x1_change = 0       
-y1_change = 0
-
+black = (0, 0, 0)
+red = (255, 0, 0)
+blue = (0, 0, 255)
+green = (0, 255, 0)
+ 
+dis_width = 800
+dis_height = 600
+ 
+dis = pygame.display.set_mode((dis_width, dis_height))
+pygame.display.set_caption('Snake Game')
+ 
 clock = pygame.time.Clock()
+ 
+snake_block = 20
 snake_speed = 10
-
-font_style = pygame.font.SysFont(None, 50)
-
-def message(msg,color):
+ 
+font_style = pygame.font.SysFont(None, 30)
+ 
+ 
+def message(msg, color):
     mesg = font_style.render(msg, True, color)
-    dis.blit(mesg, [400, 300])
-
-while not game_over:
-    for event in pygame.event.get():
-        if event.type==pygame.QUIT:
-            game_over=True
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                x1_change = -20
-                y1_change = 0
-            elif event.key == pygame.K_RIGHT:
-                x1_change = 20
-                y1_change = 0
-            elif event.key == pygame.K_UP:
-                y1_change = -20
-                x1_change = 0
-            elif event.key == pygame.K_DOWN:
-                y1_change = 20
-                x1_change = 0
-
-    if x1 >= 800 or x1 <= 0 or y1 >= 600 or y1 <= 0:
-        game_over = True
+    dis.blit(mesg, [dis_width/3, dis_height/3])
  
-    x1 += x1_change
-    y1 += y1_change
-    dis.fill(black)
-    pygame.draw.rect(dis, green, [x1, y1, snake_block, snake_block])
  
-    pygame.display.update()
+def gameLoop():
+    game_over = False
+    game_close = False
  
-    clock.tick(snake_speed)
-
-message("LOST",red)
-pygame.display.update()
-time.sleep(2)
-pygame.quit()
-quit()
+    x1 = dis_width / 2
+    y1 = dis_height / 2
+ 
+    x1_change = 0
+    y1_change = 0
+ 
+    
+    foodx = round(random.randrange(0, dis_width - snake_block) / 10.0) * 10.0
+    foody = round(random.randrange(0, dis_width - snake_block) / 10.0) * 10.0
+    
+    while not game_over:
+        while game_close == True:
+            dis.fill(black)
+            message("You Lost! Press Q-Quit or C-Play Again", red)
+            pygame.display.update()
+ 
+            for event in pygame.event.get():
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_q:
+                        game_over = True
+                        game_close = False
+                    if event.key == pygame.K_c:
+                        gameLoop()
+ 
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                game_over = True
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    x1_change = -snake_block
+                    y1_change = 0
+                elif event.key == pygame.K_RIGHT:
+                    x1_change = snake_block
+                    y1_change = 0
+                elif event.key == pygame.K_UP:
+                    y1_change = -snake_block
+                    x1_change = 0
+                elif event.key == pygame.K_DOWN:
+                    y1_change = snake_block
+                    x1_change = 0
+ 
+        if x1 >= dis_width or x1 < 0 or y1 >= dis_height or y1 < 0:
+            game_close = True
+ 
+        x1 += x1_change
+        y1 += y1_change
+        dis.fill(black)
+        pygame.draw.rect(dis, red, [foodx, foody, snake_block, snake_block])
+        pygame.draw.rect(dis, green, [x1, y1, snake_block, snake_block])
+        pygame.display.update()
+ 
+        if x1 == foodx and y1 == foody:
+            print("Eaten!!")
+        clock.tick(snake_speed)
+ 
+    pygame.quit()
+    quit()
+ 
+ 
+gameLoop()
